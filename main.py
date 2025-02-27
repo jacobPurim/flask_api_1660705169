f1eb2116951f1ec29b03af3fa7f5d3dfe148dc27
jacobPurim <133292638+jacobPurim@users.noreply.github.com>
2025-02-27 20:14:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello,Bu 1660705169'
+    return 'Hello,Bu 3 1660705169'
 
 if __name__ == '__main__':
     app.run(debug=True port=80)
